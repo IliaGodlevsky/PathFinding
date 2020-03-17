58e4c02f8b7f3f59bcfe6ef225fc99aaec182d1d
IliaGodlevsky <godleevsky2015@gmail.com>
2020-03-17 23:31:00 +0300

    some fixes

---
+++ SearchAlgorythms/SearchAlgorythms/ButtonExtension/ButtonExtension.cs
@@ -0,0 +1,39 @@
+﻿using SearchAlgorythms.Top;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SearchAlgorythms.ButtonExtension
+{
+    public static class ButtonExtension
+    {
+        public static bool IsObstacle(this Button button)
+        {
+            return button as GraphTop == null;
+        }
+
+        public static GraphTopInfo GetInfo(this Button button)
+        {
+            return new GraphTopInfo(button);
+        }
+
+        public static void MarkAsVisited(this Button button)
+        {
+            button.BackColor = Color.FromName("Yellow");
+        }
+
+        public static void MarkAsPath(this Button button)
+        {
+            button.BackColor = Color.FromName("Cyan");
+        }
+
+        public static void MarkAsObstacle(this Button button)
+        {
+            button.BackColor = Color.FromName("Black");
+        }
+
+        public static void MarkAsGraphTop(this Button button)
+        {
+            button.BackColor = Color.FromName("White");
+        }
+    }
+}
