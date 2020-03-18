7eb9e4cb46e2612c2e01498cadd19969f460d456
IliaGodlevsky <godleevsky2015@gmail.com>
2020-03-18 10:38:01 +0300

    Update WideSearch.cs

--- SearchAlgorythms/SearchAlgorythms/Algorythms/SearchAlgorythm/WideSearch.cs
+++ SearchAlgorythms/SearchAlgorythms/Algorythms/SearchAlgorythm/WideSearch.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using SearchAlgorythms.ButtonExtension;
