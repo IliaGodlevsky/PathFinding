73f2fce65f82c7b6af505d57720bc17995ddd555
IliaGodlevsky <godleevsky2015@gmail.com>
2020-03-28 10:06:19 +0300

    Update IGraphTopRoleChanger.cs

--- SearchAlgorythms/SearchAlgorythms/RoleChanger/IGraphTopRoleChanger.cs
+++ SearchAlgorythms/SearchAlgorythms/RoleChanger/IGraphTopRoleChanger.cs
@@ -1,9 +1,5 @@
 ﻿using SearchAlgorythms.Top;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SearchAlgorythms.RoleChanger
 {
