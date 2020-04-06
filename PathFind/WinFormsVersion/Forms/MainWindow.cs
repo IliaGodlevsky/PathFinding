8ff13f2d8371a614a840867ae68a017c1c6846bc
IliaGodlevsky <godleevsky2015@gmail.com>
2020-04-06 21:03:59 +0300

    Update MainWindow.cs

--- PathFind/WinFormsVersion/Forms/MainWindow.cs
+++ PathFind/WinFormsVersion/Forms/MainWindow.cs
@@ -45,9 +45,9 @@ namespace SearchAlgorythms
             Create(sender, e);
         }
 
-        private bool IsRightDestination(WinFormsVertex top)
+        private bool IsRightDestination(WinFormsVertex vertex)
         {
-            return !top.Neighbours.Any() && top.IsSimpleVertex;
+            return !vertex.Neighbours.Any() && vertex.IsSimpleVertex;
         }
 
         private void AddGraphToControls()
@@ -60,8 +60,8 @@ namespace SearchAlgorythms
                 (top as WinFormsVertex).MouseClick += changer.ChangeTopText;
                 Field.Controls.Add(top as WinFormsVertex);
             }
-            Field.Size = new Size(new Point(graph.Width*
-                Const.SIZE_BETWEEN_VERTICES, graph.Height* Const.SIZE_BETWEEN_VERTICES));
+            Field.Size = new Size(new Point(graph.Width *
+                Const.SIZE_BETWEEN_VERTICES, graph.Height * Const.SIZE_BETWEEN_VERTICES));
             Field.Location = new Point(177, 35);
             Size = new Size(Field.Size.Width + FieldParams.Width + 50,
                 Field.Size.Height + 80);
