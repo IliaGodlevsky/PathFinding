3d8d40253871f2b5caf7f2a6702314dc9b07748b
IliaGodlevsky <godleevsky2015@gmail.com>
2020-04-11 21:38:11 +0300

    Update Const.cs

--- PathFind/GraphLibrary/Constants/Const.cs
+++ PathFind/GraphLibrary/Constants/Const.cs
@@ -2,7 +2,7 @@
 {
     public static class Const
     {
-        public const int SIZE_BETWEEN_VERTICES = 25;
+        public const int SIZE_BETWEEN_VERTICES = 26;
         public const int WIN_FORMS_VERTEX_SIZE = 25;
         public const int VISIT_PAUSE_MILLISECONDS = 2;
         public const int FIND_PROCESS_PAUSE_MILLISECONDS = 8;
