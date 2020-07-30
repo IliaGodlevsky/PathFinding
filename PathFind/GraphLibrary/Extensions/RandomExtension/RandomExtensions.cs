35b3d5ac048620ed2bd35dbfe33e8e5a9560439c
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-07-31 00:20:00 +0300

    Removed to Random method IsObstacleChance

--- PathFind/GraphLibrary/Extensions/RandomExtension/RandomExtensions.cs
+++ PathFind/GraphLibrary/Extensions/RandomExtension/RandomExtensions.cs
@@ -9,5 +9,11 @@ namespace GraphLibrary.Extensions.RandomExtension
         {
             return (rand.Next(Const.MAX_VERTEX_VALUE) + Const.MIN_VERTEX_VALUE).ToString();
         }
+        
+        public static bool IsObstacleChance(this Random rand, int percentOfObstacles)
+        {
+            const int MAX_PERCENT_OF_OBSTACLES = 100;
+            return rand.Next(MAX_PERCENT_OF_OBSTACLES) < percentOfObstacles;
+        }
     }
 }
