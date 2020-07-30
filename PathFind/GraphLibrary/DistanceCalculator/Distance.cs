9ed99b99c860f20e24b2fb37bec1f9247dcf5736
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-07-31 00:17:42 +0300

    Create Distance.cs

---
+++ PathFind/GraphLibrary/DistanceCalculator/Distance.cs
@@ -0,0 +1,16 @@
+﻿using GraphLibrary.Vertex;
+using System;
+
+namespace GraphLibrary.DistanceCalculator
+{
+    public static class Distance
+    {
+        public static double GetEuclideanDistance(IVertex from, IVertex to) 
+            => Math.Sqrt(Math.Pow(from.Location.X - to.Location.X, 2)
+                + Math.Pow(from.Location.Y - to.Location.Y, 2));
+
+        public static double GetChebyshevDistance(IVertex from, IVertex to) 
+            => Math.Max(Math.Abs(from.Location.X - to.Location.X),
+                Math.Abs(from.Location.Y - to.Location.Y));
+    }
+}
