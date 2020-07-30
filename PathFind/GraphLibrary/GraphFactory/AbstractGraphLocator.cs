32974874434511cf2978418e4839eb456c08fa89
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-07-31 01:25:59 +0300

    Create AbstractGraphLocator.cs

---
+++ PathFind/GraphLibrary/GraphFactory/AbstractGraphLocator.cs
@@ -0,0 +1,28 @@
+﻿using GraphLibrary.Extensions.MatrixExtension;
+using GraphLibrary.Vertex;
+using System.Drawing;
+
+namespace GraphLibrary.GraphFactory
+{
+    public abstract class AbstractGraphLocator
+    {
+        protected IVertex[,] vertices;
+        protected int placeBetweenVertices;
+
+        protected AbstractGraphLocator(int placeBetweenVertices)
+        {
+            this.placeBetweenVertices = placeBetweenVertices;
+        }
+
+        protected virtual IVertex SetLocation(IVertex vertex)
+        {
+            var indexes = vertices.GetIndices(vertex);
+            vertex.Location = new Point(
+                indexes.X * placeBetweenVertices,
+                indexes.Y * placeBetweenVertices);
+            return vertex;
+        }
+
+        protected abstract void SetGraph(int width, int height);
+    }
+}
