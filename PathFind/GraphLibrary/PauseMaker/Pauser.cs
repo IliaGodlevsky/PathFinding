49306e0dcd8d3ede7a276bb1b7d3037fd14ec268
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-07-31 00:16:50 +0300

    Create Pauser.cs

---
+++ PathFind/GraphLibrary/PauseMaker/Pauser.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace GraphLibrary.PauseMaker
+{
+    public delegate void Pause();
+    public class Pauser
+    {
+        public Pause PauseEvent;
+
+        public void Pause(int milliseconds)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            while (watch.ElapsedMilliseconds < milliseconds)
+                PauseEvent();
+            watch.Stop();
+        }
+    }
+}
