57c2f35c5e6e6c5bc788d1767aebad9abde760c2
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-01 13:41:32 +0300

    Create EnumDescriptionConverter.cs

---
+++ PathFind/WpfVersion/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,21 @@
+﻿using GraphLibrary.Enums.AlgorithmEnum;
+using GraphLibrary.Extensions;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfVersion.Converters
+{
+    public class AlgorithmDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Algorithms)value).GetDescription();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
