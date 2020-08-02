b82252a9ce394f4ad9dfd032ab8051037bbe0434
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-02 22:40:13 +0300

    Create IntExtensions.cs

---
+++ PathFind/GraphLibrary/Extensions/IntExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphLibrary.Extensions
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
