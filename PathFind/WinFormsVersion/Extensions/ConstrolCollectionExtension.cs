d5be6049041de77f82aa080a7b0b39d17f6f8ae8
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-26 23:14:03 +0300

    Create ConstrolCollectionExtension.cs

---
+++ PathFind/WinFormsVersion/Extensions/ConstrolCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.Control;
+
+namespace WinFormsVersion.Extensions
+{
+    public static class ConstrolCollectionExtension
+    {
+        public static void RemoveBy(this ControlCollection collection, 
+            Func<Control,bool> predicate)
+        {
+            foreach(Control control in collection)
+            {
+                if (predicate(control))
+                    collection.Remove(control);
+            }
+        }
+    }
+}
