d4b3f2becf72483f1af026e1b1c8104e8c6c6bec
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-26 23:14:06 +0300

    Create ControlExtension.cs

---
+++ PathFind/WinFormsVersion/Extensions/ControlExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using WinFormsVersion.Model;
+
+namespace WinFormsVersion.Extensions
+{
+    public static class ControlExtension
+    {
+        public static bool IsGraphField(this Control control)
+        {
+            return control.GetType() == typeof(WinFormsGraphField);
+        }
+    }
+}
