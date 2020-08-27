e2a5e44f96c0d96f7d1e7617f015128fc8b59413
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-27 22:26:26 +0300

    Update WpfVertexStatusSetter.cs

--- PathFind/WpfVersion/Model/StatusSetter/WpfVertexStatusSetter.cs
+++ PathFind/WpfVersion/Model/StatusSetter/WpfVertexStatusSetter.cs
@@ -1,21 +1,37 @@
-﻿using GraphLibrary.Extensions;
+﻿using GraphLibrary.Constants;
+using GraphLibrary.Extensions;
 using GraphLibrary.Graph;
 using GraphLibrary.StatusSetter;
 using GraphLibrary.Vertex;
 using System;
+using System.Windows.Input;
 using WpfVersion.Model.Vertex;
 
 namespace WpfVersion.Model.StatusSetter
 {
     public class WpfVertexStatusSetter : AbstractVertexStatusSetter
     {
+
         public WpfVertexStatusSetter(AbstractGraph graph) : base(graph)
-        {
+        { 
+
         }
 
         public override void ChangeVertexValue(object sender, EventArgs e)
         {
-            
+            var vertex = (sender as WpfVertex);
+            if (vertex.IsObstacle)
+                return;
+            int current = int.Parse(vertex.Text);
+            if ((e as MouseWheelEventArgs).Delta > 0)
+                current++;
+            else
+                current--;
+            if (current > Const.MAX_VERTEX_VALUE)
+                current = Const.MIN_VERTEX_VALUE;
+            else if (current < Const.MIN_VERTEX_VALUE)
+                current = Const.MAX_VERTEX_VALUE;
+            vertex.Text = current.ToString();
         }
 
         public override void SetDestinationVertex(object sender, EventArgs e)
