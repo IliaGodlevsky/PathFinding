3514f4b9f08eb87b0006d9bda358a4368b579799
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-29 21:02:43 +0300

    Update AssemblyInfo.cs

--- PathFind/WinFormsVersion/Properties/AssemblyInfo.cs
+++ PathFind/WinFormsVersion/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SearchAlgorythms")]
+[assembly: AssemblyTitle("WinFormsVersion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SearchAlgorythms")]
+[assembly: AssemblyProduct("WinFormsVersion")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
