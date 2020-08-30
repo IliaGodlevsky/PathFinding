ed07c3631f06a3c30bab3719618c6facde4c36e0
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-08-30 12:29:02 +0300

    Update WpfVertex.cs

--- PathFind/WpfVersion/Model/Vertex/WpfVertex.cs
+++ PathFind/WpfVersion/Model/Vertex/WpfVertex.cs
@@ -34,10 +34,7 @@ namespace WpfVersion.Model.Vertex
             Template = (ControlTemplate)TryFindResource("vertexTemplate");
         }
 
-        public WpfVertex(VertexInfo info) : this()
-        {
-            this.Initialize(info);
-        }
+        public WpfVertex(VertexInfo info) : this() => this.Initialize(info);
 
         public bool IsEnd { get; set; }
         public bool IsObstacle { get; set; }
