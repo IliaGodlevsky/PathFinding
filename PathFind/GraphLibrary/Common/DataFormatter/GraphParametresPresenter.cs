ec78f298c2a512414ee3848f194507bfb9ecd47f
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-09-01 23:17:39 +0300

    Create GraphParametresPresenter.cs

---
+++ PathFind/GraphLibrary/Common/DataFormatter/GraphParametresPresenter.cs
@@ -0,0 +1,13 @@
+﻿using GraphLibrary.Graph;
+
+namespace GraphLibrary
+{
+    public static class GraphParametresPresenter
+    {
+        public static string GetFormattedData(AbstractGraph graph, string format)
+        {
+            return string.Format(format, graph.Width, graph.Height,
+               graph.ObstaclePercent, graph.ObstacleNumber, graph.Size);
+        }
+    }
+}
