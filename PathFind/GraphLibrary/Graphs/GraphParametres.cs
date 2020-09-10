739b6e13ddb3cf284d2a228f64e736379dcbe639
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-09-10 19:41:19 +0300

    Update GraphParametres.cs

--- PathFind/GraphLibrary/Graphs/GraphParametres.cs
+++ PathFind/GraphLibrary/Graphs/GraphParametres.cs
@@ -6,7 +6,8 @@
         public int Height { get; }
         public int ObstaclePercent { get; }
 
-        public GraphParametres(int width, int height, int obstaclePercent)
+        public GraphParametres(int width, 
+            int height, int obstaclePercent)
         {
             Width = width;
             Height = height;
