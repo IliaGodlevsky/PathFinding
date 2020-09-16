e986cb527aa0e128f9e2aef182974aec9eb95468
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-09-16 18:09:05 +0300

    changed GraphExtensions to IGraphExtenstions

--- PathFind/GraphLibrary/Extensions/CustomTypeExtensions/IGraphExtensions.cs
+++ PathFind/GraphLibrary/Extensions/CustomTypeExtensions/IGraphExtensions.cs
@@ -1,24 +1,24 @@
 ﻿using GraphLibrary.Extensions.SystemTypeExtensions;
-using GraphLibrary.Graphs;
+using GraphLibrary.Graphs.Interface;
 using GraphLibrary.Vertex;
 
 namespace GraphLibrary.Extensions.CustomTypeExtensions
 {
-    public static class GraphExtensions
+    public static class IGraphExtensions
     {
-        public static string GetFormattedInfo(this Graph graph, string format)
+        public static string GetFormattedInfo(this IGraph graph, string format)
         {
             return string.Format(format, graph.Width, graph.Height,
                graph.ObstaclePercent, graph.ObstacleNumber, graph.Size);
         }
 
-        public static void Refresh(this Graph graph)
+        public static void Refresh(this IGraph graph)
         {
             graph.RemoveExtremeVertices();
             graph.Array.Apply(vertex => vertex.Refresh());
         }
 
-        public static void RemoveExtremeVertices(this Graph graph)
+        public static void RemoveExtremeVertices(this IGraph graph)
         {
             graph.End = NullVertex.Instance;
             graph.Start = NullVertex.Instance;
