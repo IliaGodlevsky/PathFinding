b80ab6089857158b9ec3e4ddeedecf6a3cc322fa
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-09-21 21:03:43 +0300

    Create AlgoSelectorTest.cs

---
+++ PathFind/GraphLibrary.UnitTests/Tests/AlgoSelectorTest/AlgoSelectorTest.cs
@@ -0,0 +1,34 @@
+﻿using GraphLibrary.AlgoSelector;
+using GraphLibrary.Enums;
+using GraphLibrary.Graphs;
+using GraphLibrary.PathFindingAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphLibrary.UnitTests.Tests.AlgoSelectorTest
+{
+    [TestClass]
+    public class AlgoSelectorTest
+    {
+        [TestMethod]
+        public void AlgoSelectorTest_NotValidValue_ReturnsNullAlgorithm()
+        {
+            Algorithms lee = Algorithms.LeeAlgorithm;
+            Algorithms dijkstra = Algorithms.DijkstraAlgorithm;
+            Algorithms aStar = Algorithms.AStarAlgorithm;
+            Algorithms aStarModified = Algorithms.AStarModified;
+            Algorithms greedy = Algorithms.ValueGreedyAlgorithm;
+
+            var leeAlgo = AlgorithmSelector.GetPathFindAlgorithm(lee, NullGraph.Instance);
+            var dikstraAlgo = AlgorithmSelector.GetPathFindAlgorithm(dijkstra, NullGraph.Instance);
+            var aStarAlgo = AlgorithmSelector.GetPathFindAlgorithm(aStar, NullGraph.Instance);
+            var aStarModifiedAlgo = AlgorithmSelector.GetPathFindAlgorithm(aStarModified, NullGraph.Instance);
+            var greedyAlgo = AlgorithmSelector.GetPathFindAlgorithm(greedy, NullGraph.Instance);
+
+            Assert.IsInstanceOfType(leeAlgo, typeof(LeeAlgorithm));
+            Assert.IsInstanceOfType(dikstraAlgo, typeof(DijkstraAlgorithm));
+            Assert.IsInstanceOfType(aStarAlgo, typeof(AStarAlgorithm));
+            Assert.IsInstanceOfType(aStarModifiedAlgo, typeof(AStarModified));
+            Assert.IsInstanceOfType(greedyAlgo, typeof(GreedyAlgorithm));
+        }
+    }
+}
