f3c3062637f34937880154972cce763379d7e8b9
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-09-21 21:04:05 +0300

    Create LeeAlgorithmTests.cs

---
+++ PathFind/GraphLibrary.UnitTests/Tests/PathFindingAlgorithmsTests/LeeAlgorithmTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using GraphLibrary.Extensions.CustomTypeExtensions;
+using GraphLibrary.Graphs;
+using GraphLibrary.Graphs.Interface;
+using GraphLibrary.PathFindingAlgorithm;
+using GraphLibrary.UnitTests.Classes;
+using GraphLibrary.Vertex.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphLibrary.UnitTests.Tests.PathFindingAlgorithmsTests
+{
+    [TestClass]
+    public class LeeAlgorithmTests
+    {
+        [TestMethod]
+        public void LeeAlgorithmFindPath_NullGraph_ReturnsEmptyPath()
+        {
+            var leeAlgorithm = new LeeAlgorithm() { Graph = NullGraph.Instance };
+
+            leeAlgorithm.FindPath();
+
+            Assert.IsFalse(leeAlgorithm.GetPath().Any());
+        }
+
+        [TestMethod]
+        public void LeeAlgorithmFindPath_NotNullGraph_ReturnsNotEmptyPath()
+        {
+            var startVertexPosition = new Position(1, 1);
+
+            var endVertexPosition = new Position(9, 9);
+
+            var parametres = new GraphParametres(width: 10, height: 10, obstaclePercent: 0);
+            var factory = new GraphFactory.GraphFactory(parametres);
+
+            var graph = factory.GetGraph(() => new TestVertex());
+
+            graph.Start = graph[startVertexPosition.X, startVertexPosition.Y];
+            graph.End = graph[endVertexPosition.X, endVertexPosition.Y];
+
+            var leeAlgorithm = new LeeAlgorithm() { Graph = graph };
+
+            leeAlgorithm.FindPath();
+
+            Assert.IsTrue(leeAlgorithm.GetPath().Any());
+        }
+    }
+}
