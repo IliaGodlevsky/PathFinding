586dd3e186c8851a992f8cddd528b7bbf5384bb9
IliaGodlevsky <godleevsky2015@gmail.com>
2020-10-03 11:17:00 +0300

    Update AStartModified.cs

--- PathFind/GraphLibrary/PathFindingAlgorithm/AStartModified.cs
+++ PathFind/GraphLibrary/PathFindingAlgorithm/AStartModified.cs
@@ -1,4 +1,8 @@
-﻿using GraphLibrary.Vertex.Interface;
+﻿using GraphLibrary.ValueRanges;
+using GraphLibrary.Vertex;
+using GraphLibrary.Vertex.Interface;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphLibrary.PathFindingAlgorithm
 {
@@ -8,15 +12,38 @@ namespace GraphLibrary.PathFindingAlgorithm
     /// </summary>
     public class AStarModified : AStarAlgorithm
     {
+        private int persentOfFurthestToDelete;
+        /// <summary>
+        /// Percent in percent points, f.e. 5, 10, 15
+        /// </summary>
+        public int PersentOfFurthestToDelete
+        {
+            get => persentOfFurthestToDelete;
+            set
+            {
+                persentOfFurthestToDelete = value;
+                if (!percentRange.IsInBounds(persentOfFurthestToDelete))
+                    persentOfFurthestToDelete = 
+                        percentRange.ReturnInBounds(persentOfFurthestToDelete);
+            }
+        }
+
         public AStarModified() : base()
         {
-
+            deletedVertices = new List<IVertex>();
+            percentRange = new ValueRange(99, 0);
+            PersentOfFurthestToDelete = 5;
         }
 
         protected override IVertex GetChippestUnvisitedVertex()
         {
+            IVertex next = NullVertex.Instance;
             verticesProcessQueue.Sort(CompareByHeuristic);
-            verticesProcessQueue.RemoveRange(0, verticesProcessQueue.Count / 20);
+            deletedVertices.AddRange(verticesProcessQueue.Take(UpperRange));
+            verticesProcessQueue.RemoveRange(0, UpperRange);
+            next = base.GetChippestUnvisitedVertex();
+            if (ReferenceEquals(next, NullVertex.Instance))
+                verticesProcessQueue = deletedVertices;
             return base.GetChippestUnvisitedVertex();
         }
 
@@ -24,5 +51,10 @@ namespace GraphLibrary.PathFindingAlgorithm
         {
             return HeuristicFunction(vertex2).CompareTo(HeuristicFunction(vertex1));
         }
+
+        private int UpperRange => verticesProcessQueue.Count * PersentOfFurthestToDelete / 100;
+
+        private ValueRange percentRange;
+        private List<IVertex> deletedVertices;
     }
 }
