6164541809f53b39e933dcf5247ed75f0438e905
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-10-11 11:41:22 +0300

    Update DistanceCalculator.cs

--- PathFind/GraphLibrary/DistanceCalculating/DistanceCalculator.cs
+++ PathFind/GraphLibrary/DistanceCalculating/DistanceCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using GraphLibrary.Vertex.Interface;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GraphLibrary.DistanceCalculating
@@ -13,9 +14,9 @@ namespace GraphLibrary.DistanceCalculating
             var fromCoordinates = from.Position.Coordinates.ToArray();
             var toCoordinates = to.Position.Coordinates.ToArray();
             var count = fromCoordinates.Length;
-            var result = new int[fromCoordinates.Length];
+            var result = new List<int>();
             for (var i = 0; i < count; i++)
-                result[i] = Math.Abs(fromCoordinates[i] - toCoordinates[i]);
+                result.Add(Math.Abs(fromCoordinates[i] - toCoordinates[i]));
             return result.Max();
         }
     }
