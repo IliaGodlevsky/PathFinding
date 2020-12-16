03802ad5bb2db39638863ff7ca26a14017ce177a
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-16 21:58:56 +0300

    Update WpfVertex3D.cs

--- PathFind/Wpf3dVersion/Model/WpfVertex3D.cs
+++ PathFind/Wpf3dVersion/Model/WpfVertex3D.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using Wpf3dVersion.Factories;
