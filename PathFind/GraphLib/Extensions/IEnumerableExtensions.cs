47e6dd450b06994f7dc54f65a715e2d9276faa4c
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-24 12:31:17 +0300

    Create IEnumerableExtensions.cs

---
+++ PathFind/GraphLib/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphLib.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// At each step skips the number of elements equal 
+        /// to the number of loop iterations and applies an 
+        /// aggregate function to the remaining elements
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="func"></param>
+        /// <returns>Result of aggregation</returns>
+        internal static IEnumerable<T> StepAggregate<T>(this IEnumerable<T> collection, Func<T, T, T> func)
+        {
+            for (int i = 0; i < collection.Count(); i++)
+            {
+                yield return collection.Skip(i).Aggregate(func);
+            }
+        }
+    }
+}
