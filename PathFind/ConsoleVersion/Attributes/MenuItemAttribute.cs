87d7e5220a10e5d5f32d8aadbfc96d394e5ae46e
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-28 22:46:51 +0300

    Create MenuItemAttribute.cs

---
+++ PathFind/ConsoleVersion/Attributes/MenuItemAttribute.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleVersion.Enums;
+using System;
+
+namespace ConsoleVersion.Attributes
+{
+    /// <summary>
+    /// Indicates that a method can be used to create a menu item
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    internal class MenuItemAttribute : Attribute
+    {
+        public MenuItemAttribute(string description, 
+            MenuItemPriority menuItemPriority = MenuItemPriority.Normal)
+        {
+            MenuItemName = description;
+            MenuItemPriority = menuItemPriority;
+        }
+
+        public MenuItemPriority MenuItemPriority { get; private set; }
+
+        public string MenuItemName { get; private set; }
+    }
+}
