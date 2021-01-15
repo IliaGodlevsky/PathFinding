1ebf50203f9ff27d5190603f1e6d8fe00aae908b
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2021-01-15 10:08:49 +0300

    Update MainView.cs

--- PathFind/ConsoleVersion/View/MainView.cs
+++ PathFind/ConsoleVersion/View/MainView.cs
@@ -3,7 +3,6 @@ using ConsoleVersion.View.Interface;
 using ConsoleVersion.ViewModel;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using Console = Colorful.Console;
 
