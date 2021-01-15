84183ee47a0e16a7c075a30f0b05c5ff29f27900
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2021-01-15 23:02:00 +0300

    refactoring

--- PathFind/WPFVersion/ViewModel/PathFindingViewModel.cs
+++ PathFind/WPFVersion/ViewModel/PathFindingViewModel.cs
@@ -3,15 +3,32 @@ using Common.Interfaces;
 using GraphLib.ViewModel;
 using GraphViewModel.Interfaces;
 using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using WPFVersion.Infrastructure;
 
 namespace WPFVersion.ViewModel
 {
-    internal class PathFindingViewModel : PathFindingModel, IViewModel
+    internal class PathFindingViewModel : PathFindingModel, IViewModel, INotifyPropertyChanged
     {
         public event EventHandler OnWindowClosed;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<string> algorithmKeys;
+        public ObservableCollection<string> AlgorithmKeys 
+        {
+            get => algorithmKeys;
+            set { algorithmKeys = value; OnPropertyChanged(); }
+        }
 
         public ICommand ConfirmPathFindAlgorithmChoice { get; }
         public ICommand CancelPathFindAlgorithmChoice { get; }
@@ -22,9 +39,12 @@ namespace WPFVersion.ViewModel
                 ExecuteConfirmPathFindAlgorithmChoice,
                 CanExecuteConfirmPathFindAlgorithmChoice);
 
-            CancelPathFindAlgorithmChoice = new RelayCommand(obj => CloseWindow(), obj => true);
+            CancelPathFindAlgorithmChoice = new RelayCommand(obj => CloseWindow());
+
+            AlgorithmKeys = new ObservableCollection<string>(AlgorithmFactory.AlgorithmsDescriptions);
+            
             pauseProvider.PauseEvent += () => System.Windows.Forms.Application.DoEvents();
-        }        
+        }
 
         private void CloseWindow()
         {
@@ -39,7 +59,7 @@ namespace WPFVersion.ViewModel
 
         private bool CanExecuteConfirmPathFindAlgorithmChoice(object param)
         {
-            return AlgorithmFactory.AlgorithmsDescriptions.Any(algo => algo == AlgorithmKey);
+            return AlgorithmKeys.Any(algo => algo == AlgorithmKey);
         }
     }
 }
