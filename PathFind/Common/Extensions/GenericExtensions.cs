ea4f80a463a905f89186eacef70e070dd7000889
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2021-05-06 23:39:09 +0300

    Update GenericExtensions.cs

--- PathFind/Common/Extensions/GenericExtensions.cs
+++ PathFind/Common/Extensions/GenericExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using Common.Attributes;
 using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Common.Extensions
 {
@@ -18,37 +16,6 @@ namespace Common.Extensions
         public static bool IsSerializable<T>(this T self)
         {
             return self.HasAttribute<SerializableAttribute, T>();
-        }
-
-        /// <summary>
-        /// Creates a deep copy of an object
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="self"></param>
-        /// <returns>A deep copy (new instance) of an object</returns>
-        /// <exception cref="ArgumentException"></exception>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <remarks>The object what <paramref name="self"/> references at 
-        /// must be marked with attribute <see cref="SerializableAttribute"/></remarks>
-        public static T TryCopyDeep<T>(this T self)
-        {
-            if (!(self is object))
-            {
-                throw new ArgumentNullException(nameof(self));
-            }
-
-            if (!self.IsSerializable())
-            {
-                throw new ArgumentException($"{typeof(T).Name} must be serializable");
-            }
-
-            var formatter = new BinaryFormatter();
-            using (var stream = new MemoryStream())
-            {
-                formatter.Serialize(stream, self);
-                stream.Seek(0, SeekOrigin.Begin);
-                return (T)formatter.Deserialize(stream);
-            }
         }
 
         public static bool IsNullObject<T>(this T self)
