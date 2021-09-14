cab8f57c1affe1013d7f49b888a17b10dd067d1c
Ilya Hadleuski <ilha@leapwork.com>
2021-09-14 20:14:49 +0300

    ref

--- PathFind/Algorithm/Algorithm.Algos/Enums/Algorithms.cs
+++ PathFind/Algorithm/Algorithm.Algos/Enums/Algorithms.cs
@@ -7,27 +7,27 @@ namespace Algorithm.Algos.Enums
     public enum Algorithms : ulong
     {
         [Description("Lee algorithm")]
-        LeeAlgorithm = 0,
+        LeeAlgorithm = 2 << 0,
 
         [Description("Lee algorithm (heuristic)")]
-        BestFirstLeeAlgorithm = 2 << 0,
+        BestFirstLeeAlgorithm = 2 << 1,
 
         [Description("Cost-first algorithm")]
-        CostGreedyAlgorithm = 2 << 1,
+        CostGreedyAlgorithm = 2 << 2,
 
         [Description("Depth-first algorithm")]
-        DepthFirstAlgorithm = 2 << 2,
+        DepthFirstAlgorithm = 2 << 3,
 
         [Description("Distance-first algorithm")]
-        DistanceFirstAlgorithm = 2 << 3,
+        DistanceFirstAlgorithm = 2 << 4,
 
         [Description("Dijkstra's algorithm")]
-        DijkstraAlgorithm = 2 << 4,
+        DijkstraAlgorithm = 2 << 5,
 
         [Description("A* algorithm")]
-        AStarAlgorithm = 2 << 5,
+        AStarAlgorithm = 2 << 6,
 
         [Description("A* algorithm (modified)")]
-        AStarModifiedAlgorithm = 2 << 6
+        AStarModifiedAlgorithm = 2 << 7
     }
 }
