488ed5e3f63832e50b63729f6886a202e37f436e
Ilya Hadleuski <ilha@leapwork.com>
2022-02-26 22:53:25 +0300

    some fixes

--- PathFind/Algorithm/Algorithm.Interfaces/IGraphPath.cs
+++ PathFind/Algorithm/Algorithm.Interfaces/IGraphPath.cs
@@ -3,29 +3,12 @@ using System.Collections.Generic;
 
 namespace Algorithm.Interfaces
 {
-    /// <summary>
-    /// Represents path, that is 
-    /// found by pathfinding algorithm
-    /// </summary>
     public interface IGraphPath
     {
-        /// <summary>
-        /// An array of vertices, that represents a found path.
-        /// Vertices are arranged in order they were visited
-        /// by a pathfinding algorithm
-        /// </summary>
         IReadOnlyList<IVertex> Path { get; }
 
-        /// <summary>
-        /// A length of algorithms, or a number of steps that
-        /// should be done to come from the source vertex to 
-        /// the target one
-        /// </summary>
         int Length { get; }
 
-        /// <summary>
-        /// A sum of step costs of vertices in the founded path
-        /// </summary>
         double Cost { get; }
     }
 }
