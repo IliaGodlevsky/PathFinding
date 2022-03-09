cf9b9a399d33a56b61a3a03b31c94eba776b19ce
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2022-03-09 22:04:30 +0300

    some fixes

--- PathFind/Algorithm/Algorithm.Interfaces/IAlgorithm.cs
+++ PathFind/Algorithm/Algorithm.Interfaces/IAlgorithm.cs
@@ -1,15 +1,7 @@
 ﻿namespace Algorithm.Interfaces
 {
-    /// <summary>
-    /// An interface for all pathfinding algorithms
-    /// </summary>
     public interface IAlgorithm
     {
-        /// <summary>
-        /// Finds path in the graph 
-        /// or in the other data structure
-        /// </summary>
-        /// <returns></returns>
         IGraphPath FindPath();
     }
 }
