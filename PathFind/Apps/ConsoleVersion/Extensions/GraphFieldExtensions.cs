9c73420068f7d7ced2d1e5a6be3404024938f5ce
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2022-03-13 22:20:26 +0300

    some fixes

--- PathFind/Apps/ConsoleVersion/Extensions/GraphFieldExtensions.cs
+++ PathFind/Apps/ConsoleVersion/Extensions/GraphFieldExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Common.Extensions;
 using ConsoleVersion.Interface;
 using ConsoleVersion.Model;
-using ConsoleVersion.Model.FramedAxes;
 using GraphLib.Realizations.Graphs;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +11,10 @@ namespace ConsoleVersion.Extensions
     {
         public static IReadOnlyCollection<IDisplayable> GenerateDisplayables(this GraphField field, Graph2D graph)
         {
-            return field.GetAttached<FramedAxis>(graph).Concat(graph.Vertices.OfType<IDisplayable>()).ToArray();
+            return field
+                .GetAttached<IDisplayable>(graph)
+                .Concat(graph.Vertices.Cast<IDisplayable>())
+                .ToArray();
         }
     }
 }
