96ba05c2fda14fe438d1385790b2cb782c02f41e
Ilya Hadleuski <ilha@leapwork.com>
2022-04-12 09:54:05 +0300

    created conic and cylindric model for 3D vertices

--- PathFind/Apps/WPFVersion3D/Extensions/ValueTupleExtensions.cs
+++ PathFind/Apps/WPFVersion3D/Extensions/ValueTupleExtensions.cs
@@ -14,5 +14,15 @@ namespace WPFVersion3D.Extensions
 
             return new GeometryModel3D(mesh, material);
         }
+
+        public static Model3D CreateTriangleModel(this (Point3D p0, Point3D p1, Point3D p2) points, Material material)
+        {
+            var mesh = new MeshGeometry3D();
+
+            mesh.Positions.AddRange(points.p0, points.p1, points.p2);
+            mesh.TriangleIndices.AddRange(0, 1, 2);
+
+            return new GeometryModel3D(mesh, material) { BackMaterial = material };
+        }
     }
 }
---
+++ PathFind/Apps/WPFVersion3D/Model3DFactories/ConicModel3DFactory.cs
@@ -0,0 +1,74 @@
+﻿using Common.Extensions.EnumerableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+using WPFVersion3D.Extensions;
+using WPFVersion3D.Interface;
+
+namespace WPFVersion3D.Model3DFactories
+{
+    internal sealed class ConicModel3DFactory : IModel3DFactory
+    {
+        private const int Segments = 25;
+        private const int Positions = 4;
+
+        private const double Height = 6;
+        private const double HalfHeight = Height / 2;
+
+        private const double PI = 180;
+        private const double PI2 = 360;
+
+        private Vector3D center;
+
+        public Model3D CreateModel3D(double diametre, Material material)
+        {
+            double radius = diametre / 2;
+            center = new Vector3D(radius, radius, radius);
+            var conus = new Model3DGroup();
+            var points = GetPositions(0, radius);
+            var rectangles = GetRectangleModels(points, material);
+            conus.Children.AddRange(rectangles);
+            return conus;
+        }
+
+        private Point3D[,] GetPositions(double rtop, double rbottom)
+        {
+            var points = new Point3D[Segments + 1, Positions];
+            for (int i = 0; i < Segments + 1; i++)
+            {
+                points[i, 0] = GetPosition(rtop, i * PI2 / (Segments - 1), HalfHeight);
+                points[i, 1] = GetPosition(rbottom, i * PI2 / (Segments - 1), -HalfHeight);
+                points[i, 2] = GetPosition(0, i * PI2 / (Segments - 1), -HalfHeight);
+                points[i, 3] = GetPosition(0, i * PI2 / (Segments - 1), HalfHeight);
+            }
+            return points;
+        }
+
+        private IEnumerable<Model3D> GetRectangleModels(Point3D[,] points, Material material)
+        {
+            for (int i = 0; i < Segments; i++)
+            {
+                var p0 = points[i, 0];
+                var p1 = points[i, 1];
+                var p2 = points[i, 2];
+                var p3 = points[i, 3];
+                var p4 = points[i + 1, 0];
+                var p5 = points[i + 1, 1];
+
+                yield return (p0, p4, p3).CreateTriangleModel(material);
+                yield return (p1, p5, p2).CreateTriangleModel(material);
+                yield return (p0, p1, p5).CreateTriangleModel(material);
+                yield return (p0, p5, p4).CreateTriangleModel(material);
+            }
+        }
+
+        private Point3D GetPosition(double radius, double theta, double y)
+        {
+            double sn = Math.Sin(theta * Math.PI / 180);
+            double cn = Math.Cos(theta * Math.PI / 180);
+            double x = radius * cn;
+            double z = -radius * sn;
+            return new Point3D(x, y, z) + center;
+        }
+    }
+}
