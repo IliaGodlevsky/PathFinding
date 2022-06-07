c2164f26568050c28ae830b757089cd4a2669f3d
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2022-06-07 09:48:46 +0300

    Revert "some fixes"
    This reverts commit e83d6c39b354ad48f77e87b7326cba1ab16596f5.

--- PathFind/Apps/ConsoleVersion/Model/Answer.cs
+++ PathFind/Apps/ConsoleVersion/Model/Answer.cs
@@ -1,6 +1,4 @@
-﻿using Common.Extensions.EnumerableExtensions;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using ValueRange;
 
 namespace ConsoleVersion.Model
@@ -15,14 +13,11 @@ namespace ConsoleVersion.Model
 
         public static readonly InclusiveValueRange<Answer> Range = new InclusiveValueRange<Answer>(Yes, No);
 
-        private static readonly Dictionary<int, Answer> valueMap = new Dictionary<int, Answer>() { { 1, Yes}, { 0, No} };
-        private static readonly Dictionary<string, Answer> nameMap = new Dictionary<string, Answer>() { { "yes", Yes }, { "no", No } };
-
         private readonly int value;
         private readonly string name;
 
         private Answer(int value, string name)
-        {            
+        {
             this.value = value;
             this.name = name;
         }
@@ -45,12 +40,11 @@ namespace ConsoleVersion.Model
         {
             switch (obj)
             {
-                case int value:
-                    return value == this.value;
+                case int value: 
+                    return value.Equals(this.value);
                 case string name:
-                    return name.Equals(this.name, StringComparison.InvariantCultureIgnoreCase) || this.value.Equals(name);
-                case Answer answer:
-                    return answer == this;
+                    return name.Equals(this.name, StringComparison.InvariantCultureIgnoreCase)
+                        || this.value.ToString().Equals(name, StringComparison.InvariantCultureIgnoreCase);
                 default:
                     return false;
             }
@@ -68,19 +62,33 @@ namespace ConsoleVersion.Model
             return !(value1 == value2);
         }
 
-        public static explicit operator bool(Answer answer)
-        {
-            return answer.value > 0;
-        }
-
         public static explicit operator Answer(int answer)
         {
-            return valueMap.GetOrDefault(answer, () => Answer.None);
+            switch (answer)
+            {
+                case 0:
+                    return Answer.No;
+                case 1:
+                    return Answer.Yes;
+                default:
+                    return Answer.None;
+            }
         }
 
         public static explicit operator Answer(string answer)
         {
-            return nameMap.GetOrDefault(answer.ToLower(), () => Answer.None);
+            if (Answer.Yes.Equals(answer))
+            {
+                return Answer.Yes;
+            }
+            else if (Answer.No.Equals(answer))
+            {
+                return Answer.No;
+            }
+            else
+            {
+                return Answer.None;
+            }
         }
     }
 }
