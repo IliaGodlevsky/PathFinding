0b3c5f2ad9ddb7d6fec63dbf6c4d0eeb0c8f2cf3
Ilya Hadleuski <ilha@leapwork.com>
2023-05-17 18:13:57 +0300

    some changes

---
+++ PathFind/Pathfinding.App.Console/Model/Visualizations/Visuals/NullVisual.cs
@@ -0,0 +1,31 @@
+﻿using Pathfinding.App.Console.Interface;
+using Shared.Primitives.Single;
+using System;
+
+namespace Pathfinding.App.Console.Model.Visualizations.Visuals
+{
+    internal sealed class NullVisual : Singleton<NullVisual, IVisual>, IVisual
+    {
+        public event Action<Vertex> Visualized;
+
+        public bool Contains(Vertex vertex)
+        {
+            return false;
+        }
+
+        public void Remove(Vertex vertex)
+        {
+            
+        }
+
+        public void Visualize(Vertex vertex)
+        {
+            
+        }
+
+        private NullVisual()
+        {
+
+        }
+    }
+}
